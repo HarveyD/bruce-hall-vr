83967fb10980083dc8c8f6f8b11d9373824a0bdf
Harvey Delaney <harvey@harveydelaney.com>
2017-09-06 22:12:10 +1000

    made background the modal that slides, made sections code more extendable, added heading description

--- scripts.js
+++ scripts.js
@@ -1,37 +1,71 @@
 let viewer;
+let sections = {
+    "north-quad": "North Quad",
+    "south-quad": "South Quad"
+};
 
 $(document).ready(() => {
-    viewer = pannellum.viewer('panorama', {
-        "default": {
-            "firstScene": "north-quad",
-            "autoLoad": true
-        },
-        "scenes": {
-            'north-quad': {
-                "type": "equirectangular",
-                "panorama": "https://i.imgur.com/G7t9QD9.jpg",
-                "preview": "https://www.newton.ac.uk/files/covers/968361.jpg"
-            },
-            'south-quad': {
-                "type": "equirectangular",
-                "panorama": "https://pannellum.org/images/alma.jpg",
-                "preview": "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg"
-            }
-        }
-    });
+    initViewer();
 
-    $("#south-quad").click(function() {
-        viewer.loadScene('north-quad');
-        $("#panorama").removeClass("hide").addClass("show");
-    });
-
-    $("#north-quad").click(() => {
-        viewer.loadScene('south-quad');
-        $("#panorama").removeClass("hide").addClass("show");
+    Object.keys(sections).forEach(k => {
+        initSections(k, sections[k]);
     });
 
     $("#close").click(() => {
-        $("#panorama").removeClass("show").addClass("hide");
+        showOverlay();
     });
 });
+
+var initViewer = () => {
+    viewer = pannellum.viewer('panorama', {
+        "default": {
+            "firstScene": "south-quad"
+        },
+        "scenes": {
+            'south-quad': {
+                "type": "equirectangular",
+                "panorama": "https://pannellum.org/images/alma.jpg",
+                "preview": "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg",
+                "autoLoad": false
+            },
+            'north-quad': {
+                "type": "equirectangular",
+                "panorama": "/pac-quad.jpg",
+                "preview": "https://www.newton.ac.uk/files/covers/968361.jpg",
+                "autoLoad": false
+            }
+        }
+    });
+}
+
+var initSections = (id, description) => {
+    console.log(`#${id}`);
+    $(`#${id}`)
+    .mouseover(() => {
+        $("#description").text(description);
+    })
+    .mouseout(() => {
+        $("#description").text("");
+    });
+
+    $(`#${id}`).click(() => {
+        loadScene(id);
+    });
+}
+
+var loadScene = (scene) => {
+    hideOverlay();
+    
+    setTimeout(() => { // To wait until the slide animation completes
+        viewer.loadScene(scene);
+    }, 500);
+};
+
+var showOverlay = () => {
+    $("#background").removeClass("hide").addClass("show");
+}
+
+var hideOverlay = () => {
+    $("#background").removeClass("show").addClass("hide");
+}
 

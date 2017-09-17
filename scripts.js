5643be9aae7a3d0005f0ee2d96821334a1fa48e7
Harvey Delaney <harvey@harveydelaney.com>
2017-09-17 19:22:03 +1000

    made jquery scripts file more clean and maintainable by introducing an IFFY and removing functions from global scope

--- scripts.js
+++ scripts.js
@@ -1,5 +1,112 @@
-let viewer;
-let sections = {
+let BruceHallVR = (function(){
+    let viewer;
+    let sections;
+
+    let init = () => {
+        $('<img/>').attr('src', './bruce-background.jpg').on('load', () => {
+            $(this).remove();
+            $('#background').css('background-image', 'url(./bruce-background.jpg)');
+
+            $('#loading-screen').css('opacity', 0);
+            setTimeout(() => {
+                $('#loading-screen').remove();
+            }, 1000); // To wait for the animation to finish
+        });
+
+        Object.keys(sections).forEach(k => {
+            initSections(k, sections[k]);
+        });
+
+        $("#close").click(() => {
+            showOverlay();
+        });
+    };
+
+    let loadSections = (data) => {
+        sections = data;
+    }
+
+    let initSections = (id, description) => {
+        $('#background').append(`<div id="${id}" class="section"></div>`);
+    
+        $(`#${id}`)
+        .mouseover(() => {
+            $("#description").text(description);
+        })
+        .mouseout(() => {
+            $("#description").text("");
+        });
+    
+        $(`#${id}`).click(() => {
+            loadScene(id);
+        });
+    
+        // MOBILE
+        $('#background-mob').append(`<div id="m-${id}"> <h1> ${description} </h1> </div>`);
+        $(`#m-${id}`).click(() => {
+            loadScene(id);
+        });
+    }
+
+    let loadScene = (scene) => {
+        hideOverlay();
+        
+        setTimeout(() => { // To wait until the slide animation completes
+            if (!viewer) {
+                initViewer(scene);
+                return;
+            } 
+    
+            addAndLoadScene(scene);
+        }, 500);
+    };
+    
+    let showOverlay = () => {
+        $("#background").removeClass("hide").addClass("show");
+        $("#background-mob").removeClass("hide").addClass("show");
+    }
+    
+    let hideOverlay = () => {
+        $("#background").removeClass("show").addClass("hide");
+        $("#background-mob").removeClass("show").addClass("hide");
+    }
+    
+    let initViewer = (sceneName) => {
+        let config = {
+            "default": {
+                "firstScene": sceneName
+            },
+            "scenes": {}
+        };
+    
+        config["scenes"][sceneName] = { // As we can't add dynamic keys in the initial config creation
+            "type": "equirectangular",
+            "panorama": `./vr-photos/${sceneName}.jpg`,
+            "autoLoad": true
+        };
+    
+        viewer = pannellum.viewer('panorama', config);
+    }
+    
+    let addAndLoadScene = (scene) => {
+        viewer.addScene(scene, {
+            type: "equirectangular",
+            panorama: `./vr-photos/${scene}.jpg`,
+            autoLoad: false
+        });
+    
+        viewer.loadScene(scene);
+    };
+
+    let publicAPI = {
+        init: init,
+        loadSections: loadSections
+    }
+    
+    return publicAPI;
+})();
+
+BruceHallVR.loadSections({
     "north-quad": "North Quad",
     "south-quad": "South Quad",
     "pac-quad": "Packard Quad",
@@ -7,97 +114,8 @@ let sections = {
     "front": "Front of Bruce Hall",
     "reception": "Reception",
     "extension-lawn": "Extension Lawn"
-};
-
-$(document).ready(() => {
-    $('<img/>').attr('src', './bruce-background.jpg').on('load', function() {
-        $(this).remove();
-        $('#background').css('background-image', 'url(./bruce-background.jpg)');
-
-        $('#loading-screen').css('opacity', 0);
-        setTimeout(function() {
-            $('#loading-screen').remove();
-        }, 1000); // To wait for the animation to finish
-     });
-
-
-    Object.keys(sections).forEach(k => {
-        initSections(k, sections[k]);
-    });
-
-    $("#close").click(() => {
-        showOverlay();
-    });
 });
 
-var initSections = (id, description) => {
-    $('#background').append(`<div id="${id}" class="section"></div>`);
-
-    $(`#${id}`)
-    .mouseover(() => {
-        $("#description").text(description);
-    })
-    .mouseout(() => {
-        $("#description").text("");
-    });
-
-    $(`#${id}`).click(() => {
-        loadScene(id);
-    });
-
-    // MOBILE
-    $('#background-mob').append(`<div id="m-${id}"> <h1> ${description} </h1> </div>`);
-    $(`#m-${id}`).click(() => {
-        loadScene(id);
-    });
-}
-
-var loadScene = (scene) => {
-    hideOverlay();
-    
-    setTimeout(() => { // To wait until the slide animation completes
-        if (!viewer) {
-            initViewer(scene);
-            return;
-        } 
-
-        addAndLoad(scene);
-    }, 500);
-};
-
-var showOverlay = () => {
-    $("#background").removeClass("hide").addClass("show");
-    $("#background-mob").removeClass("hide").addClass("show");
-}
-
-var hideOverlay = () => {
-    $("#background").removeClass("show").addClass("hide");
-    $("#background-mob").removeClass("show").addClass("hide");
-}
-
-var initViewer = (sceneName) => {
-    let config = {
-        "default": {
-            "firstScene": sceneName
-        },
-        "scenes": {}
-    };
-
-    config["scenes"][sceneName] = { // As we can't add dynamic keys in the initial config creation
-        "type": "equirectangular",
-        "panorama": `./vr-photos/${sceneName}.jpg`,
-        "autoLoad": true
-    };
-
-    viewer = pannellum.viewer('panorama', config);
-}
-
-var addAndLoad = (scene) => {
-    viewer.addScene(scene, {
-        type: "equirectangular",
-        panorama: `./vr-photos/${scene}.jpg`,
-        autoLoad: false
-    });
-
-    viewer.loadScene(scene);
-};
+$(document).ready(() => {
+    BruceHallVR.init();
+});
